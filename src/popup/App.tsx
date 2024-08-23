0c9207cf3db665ae6386ac450ef9dd90d8e977e5
edsolater <edsolater@qq.com>
2024-08-23 19:55:09 +0800

    🚚 new manifest.json config

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,16 +1,15 @@
 import type { Component } from "solid-js"
 
-import logo from "./logo.svg"
 import styles from "./App.module.css"
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
+      <header class={styles.header} style={{ width: "30rem", height: "40rem" }}>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>hello world2</p>
         <a class={styles.link} href="https://github.com/solidjs/solid" target="_blank" rel="noopener noreferrer">
           Learn Solid
         </a>
